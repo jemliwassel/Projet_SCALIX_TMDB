d2034dcbd4cb7388e60dfbd94889a0a9feb85a5f
jemliwassel <wassel.jemli@etudiant-enit.utm.tn>
2023-10-24 17:25:31 +0200

    delete: delete unused fields from MovieActor class

--- src/main/scala/scalixModel/MovieActor.scala
+++ src/main/scala/scalixModel/MovieActor.scala
@@ -1,18 +1,9 @@
 package scalixModel
 
-case class MovieActor(posterPath: String,
-                      backdropPath: String,
-                      adult: Boolean,
+case class MovieActor(adult: Boolean,
                       id: Int,
-                      genreIds: List[Int],
-                      originalLanguage: String,
-                      overview: String,
                       originalTitle: String,
                       popularity: Double,
                       title: String,
-                      video: Boolean,
-                      voteAverage: Double,
-                      voteCount: Int,
-                      character: String,
-                      creditId: String,
                      )
+
